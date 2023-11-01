cd7b2263f376a3bc779e543e2d5720f4bf9f4573
zack <zack915037@gmail.com>
2023-11-01 23:52:12 +0800

    Add DBManager

---
+++ YC_House/Models/DBManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using YC_House.ViewModels;
+using Dapper;
+
+namespace YC_House.Models
+{
+    public class DBManager
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        
+        public List<StudentBasicInfoViewModel> QueryStudentInfoTable()
+        {
+            var sql = @"Select * 
+                        From StudentBasicInfo info
+                        Left Join StudentGrades grades
+	                        ON info.StudentId = grades.StudentId";
+
+            return new SqlConnection(connectionString).Query<StudentBasicInfoViewModel>(sql).ToList();
+        }
+    }
+}
